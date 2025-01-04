132a4a3599cdbd5a784256854ac427ee3a2cd69e
Ilya <ilia.petro2014@yandex.ru>
2025-01-04 20:38:14 +0400

    Add tests

--- src/test/java/ru/netology/test/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/test/CashbackHackServiceTest.java
@@ -1,5 +1,28 @@
 package ru.netology.test ;
 
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
+import static org.junit.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
-    
+    @Test
+    public void shouldCalculate() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(1_900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldNotCalculate() {
+        CashbackHackService cashback = new CashbackHackService();
+
+        int actual = cashback.remain(2_000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
